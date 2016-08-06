cd1e9990725b90d614251baac2bf59600cd43bf0
Adam Wick <awick@uhsure.com>
2016-08-06 10:10:00 -0700

    Make battery.sh work for Macs without batteries.

--- tmux/battery.sh
+++ tmux/battery.sh
@@ -6,14 +6,18 @@ function raw_data {
 
 if [ `uname` = "Darwin" ]; then
   PERC=$(raw_data | grep "InternalBattery" | awk '{print $2}' | sed -e 's/[^0-9]//g')
-  if [ $PERC -gt 50 ]; then
-    COLOR="#[fg=colour22]"
-  elif [ $PERC -gt 20 ]; then
-    COLOR="#[fg=colour58]"
+  if [ -z "$PERC" ]; then
+    echo ""
   else
-    COLOR="#[fg=colour1]"
+    if [ $PERC -gt 50 ]; then
+      COLOR="#[fg=colour22]"
+    elif [ $PERC -gt 20 ]; then
+      COLOR="#[fg=colour58]"
+    else
+      COLOR="#[fg=colour1]"
+    fi
+    echo "#[bg=colour237,bold]$COLOR $PERC#[fg=colour240]% #[bg=colour235]"
   fi
-  echo "#[bg=colour237,bold]$COLOR $PERC#[fg=colour240]% #[bg=colour235]"
 else
   echo ""
 fi

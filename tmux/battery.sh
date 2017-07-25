3c23e9693e426258c49945f00cfeaf516de64e1f
Adam Wick <awick@uhsure.com>
2017-07-25 09:59:07 -0700

    Updated battery monitor, to work with Sierra.

--- tmux/battery.sh
+++ tmux/battery.sh
@@ -5,7 +5,7 @@ function raw_data {
 }
 
 if [ `uname` = "Darwin" ]; then
-  PERC=$(raw_data | grep "InternalBattery" | awk '{print $2}' | sed -e 's/[^0-9]//g')
+  PERC=$(raw_data | grep "InternalBattery" | awk '{print $3}' | sed -e 's/[^0-9]//g')
   CHARGING=$(raw_data | grep "charging")
   DISCHARGING=$(raw_data | grep "discharging")
   if [ -z "$PERC" ]; then

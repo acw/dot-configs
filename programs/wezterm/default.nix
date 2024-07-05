125312418a37f19a863e9fe9a2ff0a5406445847
Adam Wick <awick@uhsure.com>
2024-07-04 20:17:16 -0700

    More vim and terminal tweaks.

--- programs/wezterm/default.nix
+++ programs/wezterm/default.nix
@@ -34,7 +34,7 @@ in
     extraConfig = ''
       return {
         font = wezterm.font("FiraCode Nerd Font"),
-        font_size = 14.0,
+        font_size = 13.0,
         hide_tab_bar_if_only_one_tab = true,
         window_background_opacity = 0.9,
       }

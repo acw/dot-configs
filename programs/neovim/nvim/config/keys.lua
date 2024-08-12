d713092bdabf6e996e92190e47393bbf47d9cf0f
Adam Wick <awick@uhsure.com>
2024-08-11 18:07:45 -0700

    fix which-key in neovim

--- programs/neovim/nvim/config/keys.lua
+++ programs/neovim/nvim/config/keys.lua
@@ -2,47 +2,47 @@ local which = require 'which-key'
 
 vim.g.mapleader = ' '
 
-which.register{
+which.add({
   -- telescope
-  ['<leader>b'] = { '<cmd>Telescope buffers<cr>', 'Buffers' },
-  ['<leader>f'] = { '<cmd>Telescope find_files<cr>', 'Files' },
+  { '<leader>b', '<cmd>Telescope buffers<cr>', desc = 'Buffers' },
+  {'<leader>f', '<cmd>Telescope find_files<cr>', desc = 'Files' },
   -- LSP bindngs
-  ['<leader>ls'] = {
+  {'<leader>ls',
     function()
       vim.lsp.buf.rename()
     end,
-    'LSP Rename'
+    desc = 'LSP Rename'
   },
-  ['<leader>ld'] = {
+  {'<leader>ld',
     function()
       vim.lsp.buf.definition()
     end,
-    'LSP Definition'
+    desc = 'LSP Definition'
   },
-  ['<leader>lD'] = {
+  {'<leader>lD',
     function()
       vim.lsp.buf.declaration()
     end,
-    'LSP Definition'
+    desc = 'LSP Definition'
   },
-  ['<leader>lr'] = {
+  {'<leader>lr',
     function()
       vim.lsp.buf.references()
     end,
-    'LSP Definition'
+    desc = 'LSP Definition'
   },
   -- trouble
-  ['<leader>tt'] = { '<cmd>TroubleToggle<cr>', 'Diagnostics (Trouble)' },
-  ['<leader>tn'] = {
+  {'<leader>tt', '<cmd>TroubleToggle<cr>', desc = 'Diagnostics (Trouble)' },
+  {'<leader>tn',
     function()
       require("trouble").next({skip_groups = true, jump = true})
     end,
-    'Next Trouble',
+    desc = 'Next Trouble',
   },
-  ['<leader>tp'] = {
+  {'<leader>tp',
     function()
       require("trouble").previous({skip_groups = true, jump = true})
     end,
-    'Next Trouble',
+    desc = 'Next Trouble',
   },
-}
+})

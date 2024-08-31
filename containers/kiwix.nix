dba1d2d851a226d809f59243323be59c8c4c05a0
Adam Wick <awick@uhsure.com>
2024-08-30 21:06:30 -0700

    the container works, although it'll need a proxy

--- containers/kiwix.nix
+++ containers/kiwix.nix
@@ -32,9 +32,32 @@ in {
       protocol = "tcp";
     }];
 
-    config = { lib, ... }: {
-      services.kiwix = {
+    config = { lib, pkgs, ... }: {
+      users = {
+        groups.kiwix.gid = guid_kiwix;
+        users.kiwix = {
+          isSystemUser = true;
+          group = "kiwix";
+          uid = guid_kiwix;
+        };
+      };
+
+      environment.systemPackages = [
+        pkgs.kiwix-tools
+      ];
+
+      systemd.services.kiwix = {
         enable = true;
+        description = "Kiwix local wiki server";
+        after = [ "network.target" ];
+        wantedBy = [ "default.target" ];
+
+        serviceConfig = {
+          ExecStart = "/run/current-system/sw/bin/sh -c \"${pkgs.kiwix-tools}/bin/kiwix-serve --port=8080 /data/*.zim\"";
+          Restart = "always";
+          RestartSec = 5;
+          User = "kiwix";
+        };
       };
 
       system.stateVersion = "24.05";

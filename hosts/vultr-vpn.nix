981ff6c2138d472e4170b8e9f409bd6917077009
Adam Wick <awick@uhsure.com>
2024-11-25 20:26:43 -0800

    add some hardware info to the vultr vpn setup

--- hosts/vultr-vpn.nix
+++ hosts/vultr-vpn.nix
@@ -7,6 +7,22 @@ in {
     ./vultr-vpn-name.nix
   ];
 
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = [];
+  boot.extraModulePackages = [];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  swapDevices = [ {
+    device = "/dev/disk/by-label/swap";
+  } ];
+
+  virtualisation.hypervGuest.enable = true;
+
   networking = {
     enableIPv6 = true;
     useDHCP = true;

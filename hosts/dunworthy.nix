e3ae519572a59f01a3b3c65772840e1714ab6e74
Adam Wick <awick@uhsure.com>
2025-01-05 19:58:55 -0800

    More nginx setup!

--- hosts/dunworthy.nix
+++ hosts/dunworthy.nix
@@ -180,6 +180,41 @@ in
         proxyWebsockets = true;
       };
     };
+
+    virtualHosts."git.uhsure.com" = {
+      extraConfig = "
+        proxy_set_header Connection $http_connection;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        client_max_body_size 512M;
+      ";
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3000";
+      };
+    };
+
+    virtualHosts."av.uhsure.com" = {
+      extraConfig = "
+        proxy_http_version 1.1;
+        proxy_set_header Connection $http_connection;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Protocol $scheme;
+        client_max_body_size 512M;
+      ";
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8096";
+        proxyWebsockets = true;
+      };
+    };
   };
 
   services.tailscale = {

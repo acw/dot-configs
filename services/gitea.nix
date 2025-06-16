c7e1d8d013f9890b5fc4b309f2169074030f40e7
Adam Wick <awick@uhsure.com>
2025-06-15 18:45:07 -0700

    Trying to do better with gitea, this is a pain.

--- services/gitea.nix
+++ services/gitea.nix
@@ -7,11 +7,15 @@
 
     database.type = "postgres";
     database.createDatabase = true;
+    lfs.enable = true;
+    stateDir = "/pool0/gitea";
 
     settings.server = {
       DOMAIN = "git.uhsure.com";
       ROOT_URL = "http://git.uhsure.com";
       HTTP_PORT = 3021;
+
+      STATIC_ROOT_PATH = "/pool0/gitea/data";
     };
   };
 

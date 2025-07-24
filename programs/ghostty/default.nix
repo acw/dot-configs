5a3acd130e3cd81e34ca7d2002dd54ce504adc9b
Adam Wick <awick@fastly.com>
2025-07-23 17:37:37 -0700

    Try setting fonts in home-manager, and try a new font.

--- programs/ghostty/default.nix
+++ programs/ghostty/default.nix
@@ -5,6 +5,13 @@
   ...
 }:
 {
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.geist-mono
+    nerd-fonts.lilex
+  ];
+
   programs.ghostty = lib.mkIf (!pkgs.stdenv.isDarwin) {
     enable = true;
     package = config.lib.nixGL.wrapOffload pkgs.ghostty;
@@ -15,8 +22,7 @@
       background = ffffff
       background-opacity = 0.95
       foreground = 000000
-      font-family = "Cousine Nerd Font Mono"
-      font-family-bold = "Cousine Nerd Font Mono Bold"
+      font-family = "GeistMono Nerd Font Mono"
       font-size = 14
 
       term = xterm-256color

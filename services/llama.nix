2d47350950c3d6d5e0c62bdcc09419ae09688f35
Adam Wick <awick@uhsure.com>
2025-08-15 13:39:32 -0700

    Working on Llama, still.

--- services/llama.nix
+++ services/llama.nix
@@ -3,7 +3,9 @@
 let llama_port = "9081";
 #    llama_model = "/pool0/ai/llama.cpp/L3-70B-Euryale-v2.1-Q4_K_M.gguf";
 #    llama_model = "/pool0/ai/llama.cpp/gemma-3-27b-it-abliterated.q5_k_m.gguf";
-    llama_model = "/pool0/ai/llama.cpp/L3.3-TRP-BASE-80-70b-Q8_0.gguf";
+#    llama_model = "/pool0/ai/llama.cpp/Writer-Large-2411-v2.1-Q5_K_M.gguf";
+#    llama_model = "/llama-cache/DeepSeek-V3-abliterated-Q4_K_M.gguf";
+     llama_model = "/pool0/ai/raw/Fallen-Command-A-111B-v1.1-GGUF/Fallen-Command-A-111B-v1c-Q5_K_M-00001-of-00002.gguf";
 in
 {
   environment.systemPackages = [

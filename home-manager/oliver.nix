413b937eee3c1cc04a872716d696f6b4ef3dd77e
Adam Wick <awick@fastly.com>
2025-10-28 15:07:33 -0700

    Updates

--- home-manager/oliver.nix
+++ home-manager/oliver.nix
@@ -89,19 +89,15 @@
   programs.zsh.enable = true;
 
   home.activation.copyDesktopFiles = lib.hm.dag.entryAfter ["installPackages"] ''
-    if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
-
       if [ ! -d "${config.home.homeDirectory}/.local/share/applications" ]; then
         mkdir "${config.home.homeDirectory}/.local/share/applications"
       fi
 
       if [ -d "${config.home.homeDirectory}/.local/share/applications/nix" ]; then
-        rm -rf "${config.home.homeDirectory}/.local/share/applications/nix"
+        rm -f "${config.home.homeDirectory}/.local/share/applications/nix"
       fi
 
       ln -sf "${config.home.homeDirectory}/.nix-profile/share/applications" \
         ${config.home.homeDirectory}/.local/share/applications/nix
-
-    fi
   '';
 }

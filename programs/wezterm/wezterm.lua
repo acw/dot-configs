747f3f9afd9b179c532a5d9560d4402a26daa49c
Adam Wick <awick@uhsure.com>
2026-01-16 20:36:32 -0800

    Fix some wezterm stuff.

--- programs/wezterm/wezterm.lua
+++ programs/wezterm/wezterm.lua
@@ -3,16 +3,29 @@ local config = {}
 
 config.font = wezterm.font("GeistMono Nerd Font Mono")
 config.font_size = 14
-
+config.line_height = 1.1
 config.hide_tab_bar_if_only_one_tab = false
-
 config.window_background_opacity = 0.9
+config.window_decorations = "RESIZE"
+
+local function standard_domain(name, address)
+  return {
+    name = name,
+    remote_address = address,
+    username = "awick",
+    local_echo_threshold_ms = 10,
+  }
+end
 
 config.ssh_domains = {
-  { name = "mensah",
-    remote_address = "100.71.249.5",
-    username = "awick",
-  },
+  standard_domain("mensah","100.71.249.5"),
+  standard_domain("graf", "100.76.80.63"),
+  standard_domain("origin", "100.74.8.5"),
+  standard_domain("grendel", "100.74.37.1"),
+}
+
+config.window_frame = {
+  font_size = 16.0,
 }
 
 config.leader = {
@@ -58,17 +71,20 @@ end
 
 wezterm.on("format-tab-title",
   function(tab, _, _, _ , _, max_width)
-    local title = tab.tab_index .. ": " .. tab_title(tab)
-    title = wezterm.truncate_right(title, max_width - 2)
+    local title = tab.tab_index .. " - " .. tab_title(tab)
+    local background = "#999999"
+    local foreground = "#000000"
 
     if tab.is_active then
-      return {
-        { Foreground = { Color = "Yellow" } },
-        { Text = title },
-      }
-    else
-      return { { text = title } }
+      background = "#FFFF99"
     end
+
+    return {
+      { Attribute = { Intensity = "Bold" } },
+      { Background = { Color = background } },
+      { Foreground = { Color = foreground } },
+      { Text = wezterm.truncate_right(title, max_width - 2) },
+    }
   end
 )
 
@@ -85,11 +101,17 @@ wezterm.on("update-status",
     })
 
     local meta = pane:get_metadata() or {}
+    local right = wezterm.time.now():format("%A, %B %d — %I:%M%p")
+      .. wezterm.time.now():format_utc(" (%H:%M UTC)")
+
     if meta.is_tardy then
       local msecs = meta.since_last_response_ms
-      window.set_right_status(string.format('tardy: %5fms', msecs))
+      right = right .. string.format(' [tardy: %5fms]', msecs)
     end
-  end
-)
+
+    window:set_right_status(wezterm.format {
+      { Text = right },
+    })
+  end)
 
 return config

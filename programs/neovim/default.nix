4340d1e9b679f6c2bc0e2f76bbfe517056cf3e4c
Adam Wick <awick@uhsure.com>
2026-01-26 21:00:07 -0800

    Formatting.

--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -1,21 +1,30 @@
 { pkgs, systemUse, ... }:
-let minimalInstall = systemUse == "infrastructure";
+let
+  minimalInstall = systemUse == "infrastructure";
 in
 {
-  home.packages = with pkgs; [
-    fd
-    nixd
-    tree-sitter
-  ] ++ (if minimalInstall then [ ] else [
-    clang-tools
-    cmake-language-server
-    docker-compose-language-service
-    gopls
-    lua-language-server
-    luarocks
-    pyright
-    yaml-language-server
-  ]);
+  home.packages =
+    with pkgs;
+    [
+      fd
+      nixd
+      tree-sitter
+    ]
+    ++ (
+      if minimalInstall then
+        [ ]
+      else
+        [
+          clang-tools
+          cmake-language-server
+          docker-compose-language-service
+          gopls
+          lua-language-server
+          luarocks
+          pyright
+          yaml-language-server
+        ]
+    );
 
   programs.neovim = {
     enable = true;
@@ -24,35 +33,33 @@ in
     vimAlias = true;
     vimdiffAlias = true;
 
-    plugins = with pkgs.vimPlugins; [
-      lualine-nvim
-      nvim-colorizer-lua
-      nvim-web-devicons
-      vimspector
-      nvim-dap
-      nvim-dap-ui
-      plenary-nvim
-      telescope-fzf-native-nvim
-      telescope-nvim
-      trouble-nvim
-      nvim-cmp
-      cmp-nvim-lsp
-      which-key-nvim
-      vim-fugitive
-    ] ++ (if minimalInstall then [
-#      nvim-treesitter.withPlugins (t: [
-##        t.c
-##        t.html
-##        t.javascript
-##        t.json
-#        t.rust
-##        t.toml
-##        t.typescript
-##        t.zsh
-#      ])
-    ] else [
-      nvim-treesitter.withAllGrammars
-    ]);
+    plugins =
+      with pkgs.vimPlugins;
+      [
+        lualine-nvim
+        nvim-colorizer-lua
+        nvim-web-devicons
+        vimspector
+        nvim-dap
+        nvim-dap-ui
+        plenary-nvim
+        telescope-fzf-native-nvim
+        telescope-nvim
+        trouble-nvim
+        nvim-cmp
+        cmp-nvim-lsp
+        which-key-nvim
+        vim-fugitive
+      ]
+      ++ (
+        if minimalInstall then
+          [
+          ]
+        else
+          [
+            nvim-treesitter.withAllGrammars
+          ]
+      );
   };
 
   home.file = {
